76e070df4138cf2700b37fef6d56579ae19ab5ca
julian2308 <julian.d.alvarado23@gmail.com>
2020-04-29 17:45:58 -0500

    Welcome

---
+++ src/DataTypes.java
@@ -0,0 +1,23 @@
+public class DataTypes {
+
+    public static void main(String[] args) {
+
+        int n = 1234567890;
+        long nL = 12345678901L;
+
+        double nD = 123.456789;
+        float nF =  123.45678F;
+
+        var salary = 1000;//int
+        //pension 3%
+        var pension = salary*0.03;//double
+        var totalSalary = salary - pension;
+        System.out.println(salary);
+        System.out.println(pension);
+        System.out.println(totalSalary);
+
+        var employeeName = "Julian Alvarado";
+        System.out.println("Employee: " + employeeName + " SALARAY: " + totalSalary);
+
+    }
+}
